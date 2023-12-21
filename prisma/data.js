5e54b5c8266560a1eaa0869b10b3d15769b5b33d
0xdorifto <gastaosousafaria@gmail.com>
2023-12-21 16:11:04 +0000

    Update poems on seed data

--- prisma/data.js
+++ prisma/data.js
@@ -7,7 +7,7 @@ let monuments = [
     description:
       "Belém Tower (Portuguese: Torre de Belém, literally: Bethlehem Tower), officially the Tower of Saint Vincent (Portuguese: Torre de São Vicente) is a 16th-century fortification located in Lisbon that served as a point of embarkation and disembarkation for Portuguese explorers and as a ceremonial gateway to Lisbon.It was built during the height of the Portuguese Renaissance, and is a prominent example of the Portuguese Manueline style, but it also incorporates hints of other architectural styles. The structure was built from lioz limestone and is composed of a bastion and a 30-metre (100 ft),four-storey tower.",
     image_url: "/monuments/torre-de-belem.png",
-    poem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate eu scelerisque felis imperdiet. Auctor eu augue ut lectus arcu bibendum at varius vel. Ipsum dolor sit amet consectetur adipiscing elit. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Ultricies tristique nulla aliquet enim tortor at. Mi sit amet mauris commodo quis. Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Adipiscing at in tellus integer feugiat scelerisque varius morbi. Arcu non odio euismod lacinia at quis risus sed vulputate. Nisi quis eleifend quam adipiscing vitae proin sagittis. Cras fermentum odio eu feugiat pretium nibh. In ante metus dictum at tempor commodo ullamcorper. Sit amet dictum sit amet justo donec. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Non enim praesent elementum facilisis. Fermentum dui faucibus in ornare quam viverra. Cursus metus aliquam eleifend mi in.",
+    poem: "By the sea, a tower stands fierce and high, Guardian sentinel 'neath the azure sky. Its walls, a fortress against invaders' might, In the dance of battle, it stands upright.\nStormy seas may rage, and invaders assail, But the tower stands firm, in the face of the gale.Beneath moonlit banners, the clash begins, Metal meeting metal, as the battle spins. ",
   },
   {
     name: "Mosteiro da Batalha",
@@ -17,7 +17,7 @@ let monuments = [
     description:
       "The Monastery of Batalha (Portuguese: Mosteiro da Batalha) is a Dominican convent in the municipality of Batalha, in the district of Leiria, in the Centro Region of Portugal. Originally, and officially, known as the Monastery of Saint Mary of the Victory (Portuguese: Mosteiro de Santa Maria da Vitória), it was erected in commemoration of the 1385 Battle of Aljubarrota and would serve as the burial church of the 15th-century Aviz dynasty of Portuguese royalty. It is one of the best and original examples of Late Flamboyant Gothic architecture in Portugal, intermingled with the Manueline style.",
     image_url: "/monuments/mosteiro-da-batalha.png",
-    poem: "Massa enim nec dui nunc mattis. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Eget magna fermentum iaculis eu non. Aliquet risus feugiat in ante metus dictum. Massa id neque aliquam vestibulum morbi blandit. Dui sapien eget mi proin sed libero. Venenatis a condimentum vitae sapien pellentesque habitant. Ornare massa eget egestas purus viverra. Arcu bibendum at varius vel pharetra vel turpis. Condimentum mattis pellentesque id nibh. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Vivamus arcu felis bibendum ut tristique et egestas quis ipsum. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus.",
+    poem: "In the heart of the realm, where victory sings, A temple emerges with majestic wings. Built in honor of a great battle won,Against invaders, under the radiant sun.\nMarble columns rise, tales etched in stone, In the temple's halls, victory's echo is known. A sacred space, where gratitude resides, For the warriors' valor, where destiny abides.",
   },
   {
     name: "Convento de Cristo",
@@ -27,7 +27,7 @@ let monuments = [
     description:
       "The Convent of Christ (Portuguese: Convento de Cristo/Mosteiro de Cristo) is a former Catholic convent in Tomar, Portugal. Originally a 12th-century Templar stronghold, when the order was dissolved in the 14th century the Portuguese branch was turned into the Knights of the Order of Christ, that later supported Portugal's maritime discoveries of the 15th century. The convent and castle complex are a historic and cultural monument and was listed as a UNESCO World Heritage site in 1983.",
     image_url: "/monuments/convento-de-cristo.png",
-    poem: "Id consectetur purus ut faucibus pulvinar elementum integer. Varius duis at consectetur lorem donec massa sapien. Sit amet luctus venenatis lectus magna fringilla urna. Ultrices gravida dictum fusce ut placerat orci. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Dolor sit amet consectetur adipiscing elit pellentesque. Nisi est sit amet facilisis magna etiam. Id leo in vitae turpis massa sed elementum. Id aliquet risus feugiat in ante. Adipiscing bibendum est ultricies integer quis auctor elit sed. Aliquam ultrices sagittis orci a scelerisque purus semper eget. Arcu non odio euismod lacinia at quis risus. Ultricies tristique nulla aliquet enim tortor at. Sodales ut etiam sit amet nisl purus. Feugiat in ante metus dictum at tempor. Turpis egestas integer eget aliquet. Penatibus et magnis dis parturient. Feugiat nisl pretium fusce id velit ut.",
+    poem: "Amidst the echoes of valor, a temple stands proud, A sanctuary of stone, where holy knights are endowed.Its walls adorned with symbols, etched in sacred might, A stronghold where the righteous gather in the soft moonlight.\nWithin the hallowed halls, knights clad in gleaming steel, Forge bonds of virtue, their purpose to reveal. Strategic minds converge, plans of righteousness unfold, In the temple's embrace, a fellowship untold.",
   },
   {
     name: "Templo de Diana",
@@ -37,7 +37,7 @@ let monuments = [
     description:
       "The Roman Temple of Évora (Portuguese: Templo romano de Évora), also referred to as the Templo de Diana (albeit wrongly, after Diana, the ancient Roman goddess of the moon, the hunt, and chastity) is an ancient temple in the Portuguese city of Évora (civil parish of Sé e São Pedro). The temple is part of the historical centre of the city, which was included in the classification by UNESCO as a World Heritage Site.It represents one of the most significant landmarks relating to the Roman and Lusitanian civilizations of Évora and in Portuguese territory.",
     image_url: "/monuments/templo-de-diana.png",
-    poem: "Hendrerit dolor magna eget est. Facilisis magna etiam tempor orci eu lobortis. Blandit cursus risus at ultrices mi tempus. Cras sed felis eget velit. Metus dictum at tempor commodo. Condimentum lacinia quis vel eros donec. Arcu dictum varius duis at consectetur lorem donec massa. Ultrices gravida dictum fusce ut placerat. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Ac felis donec et odio pellentesque diam. Porttitor massa id neque aliquam vestibulum morbi blandit cursus. Elementum integer enim neque volutpat ac tincidunt.",
+    poem: "In the embrace of hallowed soil, a temple rises high,A monument in memory, where echoes of a goddess lie.Its pillars, like ancient sentinels, touch the heavens above,A tribute to a deity, a symbol of eternal love.\nCarved in stone, the goddess's visage serene,Within the temple's halls, a sacred scene.Worshipers gather with hearts full of grace,In the temple's embrace, the goddess finds her place.",
   },
   {
     name: "Mosteiro de Alcobaça",
@@ -47,7 +47,7 @@ let monuments = [
     description:
       "Lorem ipsum tellus ligula quam molestie, nibh vestibulum aptent sodales vestibulum, habitant platea purus facilisis. Phasellus ullamcorper tellus pulvinar tortor at tempus blandit semper cubilia risus curae, duis fermentum integer porta sit auctor pharetra blandit ipsum. Nulla condimentum nec primis rhoncus volutpat mattis malesuada, interdum aliquam sollicitudin massa quisque euismod gravida, libero enim per ullamcorper aliquam commodo. Ut libero dapibus aenean molestie phasellus scelerisque, netus dictum etiam pharetra odio sollicitudin, lorem dictumst senectus congue etiam.",
     image_url: "/monuments/mosteiro-de-alcobaça.png",
-    poem: "Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Nec tincidunt praesent semper feugiat nibh sed pulvinar. Eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Sit amet commodo nulla facilisi. Eu nisl nunc mi ipsum faucibus vitae aliquet nec. Elit sed vulputate mi sit amet mauris commodo quis imperdiet. At in tellus integer feugiat scelerisque varius morbi. Tempus quam pellentesque nec nam aliquam. Ultrices gravida dictum fusce ut placerat orci nulla. Diam vel quam elementum pulvinar etiam. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Sit amet volutpat consequat mauris nunc congue. Pharetra et ultrices neque ornare aenean. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Nisi lacus sed viverra tellus in hac habitasse.",
+    poem: "In the heart of time, where echoes weave,An ancient temple, where destinies cleave.Clerics in robes, with solemn grace,Paladins armored, their virtues to embrace.\nTogether they stand, guardians of the shrine,The temple whispers, a tale divine.Clerics weave chants, a primordial art,Paladins shield, a defender's part.",
   },
   {
     name: "Palácio da Pena",
@@ -57,7 +57,7 @@ let monuments = [
     description:
       "Porta sodales donec quisque ultrices faucibus porta eget condimentum curae, ante consectetur pretium diam eu viverra senectus justo aenean commodo, vehicula tellus faucibus rhoncus dolor etiam interdum dictumst. Suscipit vivamus risus condimentum convallis risus sagittis non cursus, netus aliquam per dictumst sem ipsum purus etiam, enim ultricies commodo quisque tristique consectetur mollis. Nulla vulputate lacinia volutpat blandit nam erat class felis justo aenean, eleifend ut interdum nullam praesent bibendum ad ipsum urna mauris habitant, feugiat sagittis mollis nisl dictum elementum suspendisse etiam lobortis.",
     image_url: "/monuments/palacio-da-pena.png",
-    poem: "Sed risus ultricies tristique nulla. Hendrerit gravida rutrum quisque non tellus orci. Tellus in hac habitasse platea dictumst vestibulum. Id aliquet risus feugiat in ante metus. Egestas pretium aenean pharetra magna ac placerat. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Curabitur vitae nunc sed velit. Elit ut aliquam purus sit amet luctus. A scelerisque purus semper eget duis at tellus. Id faucibus nisl tincidunt eget nullam non. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Neque laoreet suspendisse interdum consectetur. Tortor at risus viverra adipiscing at in. Metus aliquam eleifend mi in nulla posuere sollicitudin.",
+    poem: "High above, in the realm of dreams,A palace veiled in clouds, where mystery gleams. Its towers touch the sky with an ethereal grace, A hidden sanctuary, an enchanting place.\nWhispers of legends in the zephyr's embrace, A celestial fortress, where time leaves no trace.Silent halls draped in mist, secrets untold, In the cloud-kissed palace, a story unfolds. ",
   },
   {
     name: "Castelo de Guimarães",
@@ -67,7 +67,7 @@ let monuments = [
     description:
       "Neque aliquam posuere eu dui orci curabitur platea, pharetra in placerat leo duis ligula nam per, lorem pulvinar mauris lacinia sit augue. Dui ornare ante at aenean consectetur orci ad non, aenean consectetur dui per mauris potenti ante conubia, quisque euismod iaculis neque feugiat eget augue. Purus mollis curae adipiscing primis est sed justo, dictum tempor diam senectus ultricies interdum imperdiet, inceptos quis ad fames feugiat eu.",
     image_url: "/monuments/castelo-de-guimaraes.png",
-    poem: "Fermentum et sollicitudin ac orci phasellus egestas tellus rutrum. Rhoncus est pellentesque elit ullamcorper dignissim. Nunc aliquet bibendum enim facilisis gravida neque convallis. Risus ultricies tristique nulla aliquet enim tortor at auctor. Tempus egestas sed sed risus pretium quam vulputate. Ultrices neque ornare aenean euismod elementum. Orci sagittis eu volutpat odio. Augue neque gravida in fermentum. Senectus et netus et malesuada fames ac turpis egestas. In mollis nunc sed id semper risus. Tortor pretium viverra suspendisse potenti nullam ac. Aliquet nibh praesent tristique magna sit amet purus gravida quis.",
+    poem: "In lands of lore, where legends dwell, a primordial castle tells its tale, Its stones laid at the dawn of time, in every brick, a story's chime.  Upon a hill, where eagles soar, it stood as guardian, lore's core,  A nation's heart, in shadows cast, shaped by echoes of the past.\nThis fortress, old, with walls so wide, bore witness to a nation's pride,  In its embrace, history unfurled, a cradle for the newborn world.  Through war and peace, its towers tall, have seen the rise and fall of all,  A silent sentinel, aged and grand, the founding stone of a thriving land.",
   },
   {
     name: "Universidade de Coimbra",
@@ -77,7 +77,7 @@ let monuments = [
     description:
       "Platea litora sit commodo a fusce auctor eros, vitae convallis faucibus odio laoreet tempor placerat accumsan, orci elit libero potenti aptent torquent. Inceptos vel tincidunt nisl venenatis sodales posuere enim tempor, bibendum tempus hendrerit nisi augue feugiat taciti hac, odio auctor ullamcorper per himenaeos odio ante. Senectus condimentum nibh aenean feugiat porta eu ac dui dictum fusce molestie faucibus himenaeos ad, purus quisque etiam nisl etiam netus habitant himenaeos elementum consequat class quis pellentesque.",
     image_url: "/monuments/universidade-de-coimbra.png",
-    poem: "Ac turpis egestas integer eget aliquet. In nulla posuere sollicitudin aliquam ultrices sagittis. Tempus urna et pharetra pharetra massa massa ultricies. Commodo odio aenean sed adipiscing. Posuere urna nec tincidunt praesent semper feugiat nibh sed. Facilisis volutpat est velit egestas dui id ornare. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Tortor vitae purus faucibus ornare suspendisse sed. Nisi scelerisque eu ultrices vitae auctor. Dolor sit amet consectetur adipiscing. Fermentum dui faucibus in ornare quam viverra orci. Gravida cum sociis natoque penatibus. Eget mi proin sed libero enim sed. Urna porttitor rhoncus dolor purus non enim praesent elementum facilisis. Diam vulputate ut pharetra sit amet aliquam. Nisl tincidunt eget nullam non nisi est sit amet. Tempor nec feugiat nisl pretium fusce id velit ut. Facilisis gravida neque convallis a cras semper auctor. Facilisi etiam dignissim diam quis enim lobortis. Tellus in hac habitasse platea dictumst vestibulum rhoncus est.",
+    poem: "In the echoes of history, an ancient school stands tall,A bastion of knowledge, the pride of all.Its halls adorned with wisdom of ages past,Where scholars delve into the realms vast.\nBooks whisper tales, penned by hands of old,In the ancient school, stories unfold.A beacon of learning, revered with pride,In the country's heart, knowledge resides.",
   },
   {
     name: "Convento de Mafra",
@@ -87,7 +87,7 @@ let monuments = [
     description:
       "Curae arcu amet phasellus scelerisque convallis libero cursus vivamus, at vitae a turpis bibendum amet etiam, lorem iaculis feugiat morbi eros nostra hendrerit. Molestie lobortis orci magna felis class pharetra etiam aenean congue egestas, posuere lacinia mauris cubilia pretium placerat vitae morbi curabitur libero, sapien morbi velit felis vitae nostra accumsan risus etiam. Faucibus consequat tellus eleifend interdum quisque consequat velit dictum lectus et sollicitudin, hac ligula vel ultrices praesent malesuada laoreet sagittis aliquam.",
     image_url: "/monuments/convento-de-mafra.png",
-    poem: "Leo a diam sollicitudin tempor id eu nisl. Aliquet eget sit amet tellus cras adipiscing enim eu. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Urna id volutpat lacus laoreet non. Mauris commodo quis imperdiet massa tincidunt nunc. Ac tortor dignissim convallis aenean et tortor at risus viverra. Eget duis at tellus at urna. Nullam eget felis eget nunc lobortis mattis aliquam. Dictum non consectetur a erat. Bibendum neque egestas congue quisque. Urna duis convallis convallis tellus id interdum velit laoreet. Viverra vitae congue eu consequat ac felis donec et.",
+    poem: "In the realm where promises take hold,A king declared, his words of gold.A temple-library, a tribute true,To a queen's grace, in skies so blue.\nMarble columns rose, a testament grand,Inscribed in stone, by a king's own hand.Books and scrolls, a treasury of lore,Gifts of wisdom, forevermore.",
   },
   {
     name: "Bom Jesus do Monte",
@@ -97,7 +97,7 @@ let monuments = [
     description:
       "The Sanctuary of Bom Jesus do Monte is a Portuguese Catholic shrine in Tenões, outside the city of Braga, in northern Portugal. Its name means Good Jesus of the Mount.This sanctuary is a notable example of Christian pilgrimage site with a monumental, Baroque stairway that climbs 116 meters (381 feet). It is an important tourist attraction of Braga.On 7 July 2019, the Sanctuary of Bom Jesus do Monte in Braga was inscribed as a UNESCO World Heritage Site.",
     image_url: "/monuments/bom-jesus-do-monte.png",
-    poem: "Leo a diam sollicitudin tempor id eu nisl. Aliquet eget sit amet tellus cras adipiscing enim eu. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Urna id volutpat lacus laoreet non. Mauris commodo quis imperdiet massa tincidunt nunc. Ac tortor dignissim convallis aenean et tortor at risus viverra. Eget duis at tellus at urna. Nullam eget felis eget nunc lobortis mattis aliquam. Dictum non consectetur a erat. Bibendum neque egestas congue quisque. Urna duis convallis convallis tellus id interdum velit laoreet. Viverra vitae congue eu consequat ac felis donec et.",
+    poem: "In reverence and faith, a temple did rise.To the one true God, beneath the heavenly skies.Jesus Christ, the cornerstone so divine,A sanctuary of worship, a sacred shrine.\nWith stained glass windows, a story unfolds,Of compassion, salvation, as the Scripture molds.Hymns of devotion in the hallowed air,In the temple's embrace, the faithful declare.",
   },
 ];
 
@@ -149,8 +149,6 @@ let users = [
     monuments: [{ id: 1 }],
   },
 ];
-
-console.log("Monuments:");
 
 module.exports = {
   monuments,
